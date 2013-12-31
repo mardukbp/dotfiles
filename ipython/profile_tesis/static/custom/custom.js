fff91e1cd474a2fc1377d0337457044c20d12fcc
marduk <marduk@ciencias.unam.mx>
2013-12-30 22:03:52 -0600

    IPython profiles

---
+++ ipython/profile_tesis/static/custom/custom.js
@@ -0,0 +1,17 @@
+$.getScript("/static/components/codemirror/keymap/vim.js", function() {
+    if (! IPython.Cell) return;
+    IPython.Cell.options_default.cm_config.keyMap = "vim";
+});
+
+$([IPython.events]).on('app_initialized.NotebookApp', function(){
+    var IPyNB = '/static/custom/IPython-notebook-extensions';
+    require(['/static/custom/codefolding/codefolding.js']);
+    require([IPyNB + '/usability/comment-uncomment.js']);
+});
+
+//<script type="text/x-mathjax-config">
+//MathJax.Hub.Config({
+//  "HTML-CSS": { linebreaks: { automatic: true } },
+//         SVG: { linebreaks: { automatic: true } }
+//});
+//</script>
