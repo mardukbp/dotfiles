d5005fb608c383cdd642f5f5154de16370dbee3a
marduk <marduk@ciencias.unam.mx>
2014-04-07 22:17:02 -0500

    Deleted custom config.

--- ipython/profile_tesis/static/custom/custom.js
+++ ipython/profile_tesis/static/custom/custom.js
@@ -1,13 +1,13 @@
-$.getScript("/static/components/codemirror/keymap/vim.js", function() {
-    if (! IPython.Cell) return;
-    IPython.Cell.options_default.cm_config.keyMap = "vim";
-});
+//$.getScript("/static/components/codemirror/keymap/vim.js", function() {
+//    if (! IPython.Cell) return;
+//    IPython.Cell.options_default.cm_config.keyMap = "vim";
+//});
 
-$([IPython.events]).on('app_initialized.NotebookApp', function(){
-    var IPyNB = '/static/custom/IPython-notebook-extensions';
-    require(['/static/custom/codefolding/codefolding.js']);
-    require([IPyNB + '/usability/comment-uncomment.js']);
-});
+//$([IPython.events]).on('app_initialized.NotebookApp', function(){
+//    var IPyNB = '/static/custom/IPython-notebook-extensions';
+//    require(['/static/custom/codefolding/codefolding.js']);
+//    require([IPyNB + '/usability/comment-uncomment.js']);
+//});
 
 //<script type="text/x-mathjax-config">
 //MathJax.Hub.Config({
